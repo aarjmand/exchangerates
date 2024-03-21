8e14ff228d95cae30caffffaafa2ef4ca8424c64
Amir Arjmand <amir.arjmand@accenture.com>
2024-03-22 02:32:13 +1100

    First draft of the analysis

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import sys
+import pandas as pd
+import matplotlib.pyplot as plt
 from helpers import *
 
 _config = None
@@ -7,14 +9,27 @@ _logger = None
 def main():
 
     endpoint = build_endpoint(_config)
-    print(endpoint)
+
+    data = get_data(endpoint)
+
+    flat_rates = [(date, currency, rate) for date, currencies in data['rates'].items() for currency, rate in currencies.items()]
+    df = pd.DataFrame(flat_rates, columns=['Date', 'Currency', 'Rate'])
+
+    rs = df.groupby(['Currency']).agg(
+        mean=('Rate', 'mean'),
+        worst=('Rate', 'min'),
+        best=('Rate', 'max')
+    )
+
+    print(rs)
 
 if __name__ == "__main__":
     _config = load_config('../conf/config.ini')
     _logger = get_logger()
 
     try:
+        _logger.info('Initializing the app!')
         main()
     except Exception as e:
-        _logger(f"An error occured: {str(e)}")
+        _logger.exception(f"An error occured: {str(e)}")
         sys.exit(1)
